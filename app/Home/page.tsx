9a79c568dc6f76e931b5347becbf52cb931b88c7
Sri Ganesh <ganeshsri38253@gmail.com>
2024-01-12 00:13:29 +0530

    commit

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,34 +1,33 @@
-'use client'
-import React from 'react'
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { getToken, deleteCookie } from '../comp/cookie';
-import Link from 'next/link';
-
+"use client";
+import React from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { getToken, deleteCookie } from "../comp/cookie";
+import Link from "next/link";
 
 export default function Home() {
-
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const token = getToken();
 
     if (!token) {
-        router.push('/Login');
+      router.push("/Login");
     }
   }, [router]);
-
 
   const handleLogout = () => {
     // Delete the cookie and redirect to the login page
     deleteCookie();
-    router.push('/Login');
+    router.push("/Login");
   };
-  console.log('Hi')
+  console.log("Hi");
   return (
     <div>
-                <h1>This is Home Page</h1>
+      <h1>This is Home Page</h1>
 
-      <Link href={'/Login'} onClick={handleLogout}>Logout</Link>
+      <Link href={"/Login"} onClick={handleLogout}>
+        Logout
+      </Link>
     </div>
-  )
+  );
 }
